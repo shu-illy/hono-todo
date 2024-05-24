dc51a74d31a5f7463e57bfe7ad1677269cdd846a
shu-illy <sh.illy.9117@gmail.com>
2024-05-25 01:27:25 +0900

    Revert "KVの動作検証"
    This reverts commit 92fe1253cecf63f352a93d3eb19d1c2b4af4187c.

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,8 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("*", renderer);
 
-app.get("/", async (c) => {
-  await c.env.MY_KV.put("name", c.env.MY_NAME);
-  const name = await c.env.MY_KV.get("name");
-  return c.render(<h1>Hello, {name}</h1>);
+app.get("/", (c) => {
+  return c.render(<h1>Hello, Cloudflare Pages!</h1>);
 });
 
 export default app;
