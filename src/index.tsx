92fe1253cecf63f352a93d3eb19d1c2b4af4187c
shu-illy <sh.illy.9117@gmail.com>
2024-05-25 01:17:41 +0900

    KVの動作検証

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,10 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("*", renderer);
 
-app.get("/", (c) => {
-  return c.render(<h1>Hello, Cloudflare Pages!</h1>);
+app.get("/", async (c) => {
+  await c.env.MY_KV.put("name", c.env.MY_NAME);
+  const name = await c.env.MY_KV.get("name");
+  return c.render(<h1>Hello, {name}</h1>);
 });
 
 export default app;
