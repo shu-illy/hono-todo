030a137809e98977836a4f72e726c8e02823ad8b
shu-illy <sh.illy.9117@gmail.com>
2024-05-25 03:03:13 +0900

    TailwindCSS導入

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,6 +1,7 @@
-import { Style } from 'hono/css'
-import { jsxRenderer } from 'hono/jsx-renderer'
-import { Script } from 'honox/server'
+import { Style } from "hono/css";
+import { jsxRenderer } from "hono/jsx-renderer";
+import { Script } from "honox/server";
+import styles from "../style.css?url";
 
 export default jsxRenderer(({ children, title }) => {
   return (
@@ -10,10 +11,11 @@ export default jsxRenderer(({ children, title }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href={styles} rel="stylesheet" />
         <Script src="/app/client.ts" async />
         <Style />
       </head>
       <body>{children}</body>
     </html>
-  )
-})
+  );
+});
