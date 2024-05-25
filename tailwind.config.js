9d725da6391ad53f6451475a634e2e81a6c0f7ee
shu-illy <sh.illy.9117@gmail.com>
2024-05-25 19:11:08 +0900

    daisyui設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./app/**/*.tsx"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography, daisyui],
 };
